fa4a40c001b42147b43f6e1fa6d23e2e812020fa
blueice1234 <42489990+blueice1234@users.noreply.github.com>
2019-12-22 01:34:18 +0800

    refactor: move some functions to HelpFunc

---
+++ Movie/HelpFunc.cpp
@@ -0,0 +1,147 @@
+#include "HelpFunc.h"
+
+void InfoToTxt(char* c)
+{
+	int i = 0;
+	while (!(c[i] >= '0' && c[i] <= '9'))
+		i++;
+	while (c[i] >= '0' && c[i] <= '9')
+		i++;
+	// c[i] = point
+	c[++i] = 't'; c[++i] = 'x'; c[++i] = 't';
+	c[++i] = '\0';
+}
+void TxtToInfo(char* c)
+{
+	int i = 0;
+	while (!(c[i] >= '0' && c[i] <= '9'))
+		i++;
+	while (c[i] >= '0' && c[i] <= '9')
+		i++;
+	// c[i] = point
+	c[++i] = 'i'; c[++i] = 'n'; c[++i] = 'f'; c[++i] = '0';
+	c[++i] = '\0';
+}
+void IdToInfo(int id, char* str)
+{
+	_itoa(id, str, 10);
+	int i = 0;
+	while (str[i] != '\0')
+		i++;
+	str[i++] = '.'; str[i++] = 'i'; str[i++] = 'n'; str[i++] = 'f'; str[i++] = 'o';
+	str[i++] = '\0';
+}
+int CharToInt(char* c)
+{
+	int result = 0;
+	int i = 0;
+	while (!(c[i] >= '0' && c[i] <= '9'))
+		i++;
+	while (c[i] >= '0' && c[i] <= '9')
+	{
+		result = result * 10 + c[i] - '0';
+		i++;
+	}
+	return result;
+}
+
+void GetFileList(CharStringLink* inFileList, const char* inPath, const char* inFormat)
+{
+	char PathFormat[100];
+	strcpy(PathFormat, inPath);
+	strcat(PathFormat, "*");
+	strcat(PathFormat, inFormat);
+
+	struct _finddata_t fileinfo;
+	intptr_t handle;
+	if ((handle = _findfirst(PathFormat, &fileinfo)) == -1)
+	{
+		cout << "No file found!" << endl;
+		system("pause");
+		exit(0);
+	}
+	else
+	{
+		do
+		{
+			char inPathFile[100];
+			strcpy(inPathFile, inPath);
+			strcat(inPathFile, fileinfo.name);
+			inFileList->add(inPathFile);
+		} while (!(_findnext(handle, &fileinfo)));
+		_findclose(handle);
+	}
+}
+
+void LoadDict(BalancedBinaryTree* dict, char* path)
+{
+	string lineData;
+	ifstream readDict(path);
+	int max_num = 275908;
+	const int max_len = 50;
+	char** temp = new char*[max_num];
+	memset(temp, 0, sizeof(char*) * max_num);
+	for (int i = 0; i < max_num; i++)
+	{
+		temp[i] = new char[max_len];
+		memset(temp[i], 0, sizeof(char) * max_len);
+	}
+	max_num = 0;
+	while (readDict.getline(temp[max_num], 50))
+	{
+		//cout << temp[max_num] << ",";
+		max_num++;
+	}
+	//cout << "dict num: " << max_num << endl;
+	readDict.close();
+	// 折半插入
+	bool adjust = false;
+	//for (int i = 0; i < max_num; i++)
+	//	dict->InsertNode(temp[i], &adjust);
+	int usedLength = 1;
+	int* nextIndex = new int[usedLength];
+	memset(nextIndex, 0, sizeof(int) * usedLength);
+	nextIndex[0] = (0 + max_num) / 2;
+	int success = 0;
+	while (success < max_num)
+	{
+		int* nextIndexTemp = new int[usedLength * 2];
+		memset(nextIndexTemp, 0, sizeof(int) * usedLength * 2);
+		int k = 0;
+		// 一边插入，一边更新下一个待插入序列
+		for (int i = 0; i < usedLength; i++)
+		{
+			if (dict->InsertNode(temp[nextIndex[i]], &adjust))
+				success++;
+			//cout << temp[nextIndex[i]] << ", ";
+			if (i == 0 && i == usedLength - 1)
+			{
+				nextIndexTemp[k++] = nextIndex[0] / 2;
+				nextIndexTemp[k++] = (nextIndex[0] + max_num) / 2;
+			}
+			else if (i == usedLength - 1 && i != 0)
+			{
+				nextIndexTemp[k++] = (nextIndex[i] + max_num) / 2;
+			}
+			else
+			{
+				if (i == 0)
+				{
+					nextIndexTemp[k++] = nextIndex[0] / 2;
+					if (usedLength == 1)
+						break;
+				}
+				int mid = (nextIndex[i] + nextIndex[i + 1] + 1) / 2;
+				nextIndexTemp[k++] = (nextIndex[i] + mid) / 2;
+				nextIndexTemp[k++] = (mid + nextIndex[i + 1]) / 2;
+			}
+		}
+		delete[] nextIndex;
+		nextIndex = nextIndexTemp;
+		usedLength *= 2;
+	}
+	for (int i = 0; i < max_num; i++)
+		delete[] temp[i];
+	delete[] temp;
+
+}
--- Movie/Movie.cpp
+++ Movie/Movie.cpp
@@ -1,5 +1,6 @@
 ﻿#include "BalancedBinaryTree.h"
 #include "CharStringLink.h"
+#include "HelpFunc.h"
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -8,152 +9,6 @@
 
 using namespace std;
 #define MAX_COMMAND 5
-
-void InfoToTxt(char* c)
-{
-	int i = 0;
-	while (!(c[i] >= '0' && c[i] <= '9'))
-		i++;
-	while (c[i] >= '0' && c[i] <= '9')
-		i++;
-	// c[i] = point
-	c[++i] = 't'; c[++i] = 'x'; c[++i] = 't';
-	c[++i] = '\0';
-}
-void TxtToInfo(char* c)
-{
-	int i = 0;
-	while (!(c[i] >= '0' && c[i] <= '9'))
-		i++;
-	while (c[i] >= '0' && c[i] <= '9')
-		i++;
-	// c[i] = point
-	c[++i] = 'i'; c[++i] = 'n'; c[++i] = 'f'; c[++i] = '0';
-	c[++i] = '\0';
-}
-void IdToInfo(int id, char* str)
-{
-	_itoa(id, str, 10);
-	int i = 0;
-	while (str[i] != '\0')
-		i++;
-	str[i++] = '.'; str[i++] = 'i'; str[i++] = 'n'; str[i++] = 'f'; str[i++] = 'o';
-	str[i++] = '\0';
-}
-inline int CharToInt(char* c)
-{
-	int result = 0;
-	int i = 0;
-	while (!(c[i] >= '0' && c[i] <= '9'))
-		i++;
-	while (c[i] >= '0' && c[i] <= '9')
-	{
-		result = result * 10 + c[i] - '0';
-		i++;
-	}
-	return result;
-}
-
-void GetFileList(CharStringLink* inFileList, const char* inPath, const char* inFormat)
-{
-	char PathFormat[100];
-	strcpy(PathFormat, inPath);
-	strcat(PathFormat, "*");
-	strcat(PathFormat, inFormat);
-
-	struct _finddata_t fileinfo;
-	intptr_t handle;
-	if ((handle = _findfirst(PathFormat, &fileinfo)) == -1)
-	{
-		cout << "No file found!" << endl;
-		system("pause");
-		exit(0);
-	}
-	else
-	{
-		do
-		{
-			char inPathFile[100];
-			strcpy(inPathFile, inPath);
-			strcat(inPathFile, fileinfo.name);
-			inFileList->add(inPathFile);
-		} while (!(_findnext(handle, &fileinfo)));
-		_findclose(handle);
-	}
-}
-
-void LoadDict(BalancedBinaryTree* dict, char* path)
-{
-	string lineData;
-	ifstream readDict(path);
-	int max_num = 275908;
-	const int max_len = 50;
-	char** temp = new char* [max_num];
-	memset(temp, 0, sizeof(char*) * max_num);
-	for (int i = 0; i < max_num; i++)
-	{
-		temp[i] = new char[max_len];
-		memset(temp[i], 0, sizeof(char) * max_len);
-	}
-	max_num = 0;
-	while (readDict.getline(temp[max_num], 50))
-	{
-		//cout << temp[max_num] << ",";
-		max_num++;
-	}
-	//cout << "dict num: " << max_num << endl;
-	readDict.close();
-	// 折半插入
-	bool adjust = false;
-	//for (int i = 0; i < max_num; i++)
-	//	dict->InsertNode(temp[i], &adjust);
-	int usedLength = 1;
-	int* nextIndex = new int[usedLength];
-	memset(nextIndex, 0, sizeof(int) * usedLength);
-	nextIndex[0] = (0 + max_num) / 2;
-	int success = 0;
-	while (success < max_num)
-	{
-		int* nextIndexTemp = new int[usedLength * 2];
-		memset(nextIndexTemp, 0, sizeof(int) * usedLength * 2);
-		int k = 0;
-		// 一边插入，一边更新下一个待插入序列
-		for (int i = 0; i < usedLength; i++)
-		{
-			if (dict->InsertNode(temp[nextIndex[i]], &adjust))
-				success++;
-			//cout << temp[nextIndex[i]] << ", ";
-			if (i == 0 && i == usedLength - 1)
-			{
-				nextIndexTemp[k++] = nextIndex[0] / 2;
-				nextIndexTemp[k++] = (nextIndex[0] + max_num) / 2;
-			}
-			else if (i == usedLength - 1 && i != 0)
-			{
-				nextIndexTemp[k++] = (nextIndex[i] + max_num) / 2;
-			}
-			else
-			{
-				if (i == 0)
-				{
-					nextIndexTemp[k++] = nextIndex[0] / 2;
-					if (usedLength == 1)
-						break;
-				}
-				int mid = (nextIndex[i] + nextIndex[i + 1] + 1) / 2;
-				nextIndexTemp[k++] = (nextIndex[i] + mid) / 2;
-				nextIndexTemp[k++] = (mid + nextIndex[i + 1]) / 2;
-			}
-		}
-		delete[] nextIndex;
-		nextIndex = nextIndexTemp;
-		usedLength *= 2;
-	}
-	for (int i = 0; i < max_num; i++)
-		delete[] temp[i];
-	delete[] temp;
-
-}
 
 int main()
 {
@@ -241,6 +96,7 @@ int main()
 		delete resultList;
 	}
 	readQuery1.close();
+	cout << "query 1 finished\n";
 
 
 	// Deal with query2.txt
@@ -295,7 +151,26 @@ int main()
 			}
 
 			// Find given aim movie
-			if (strcmp(movieTitle, infoTitle) == 0)
+			bool same = true;
+			if (movieTitle[0] != infoTitle[0] || movieTitle[1] != infoTitle[1])
+				same = false;
+			else
+			{
+				size_t len1 = strlen(movieTitle);
+				size_t len2 = strlen(infoTitle);
+				len1 = len1 <= len2 ? len1 : len2;
+				for (size_t i = 0; i < len1; i++)
+				{
+					if (movieTitle[i] != infoTitle[i])
+					{
+						same = false;
+						break;
+					}
+					else if (movieTitle[i] == ' ' || infoTitle[i] == ' ')
+						break;
+				}
+			}
+			if (same)
 			{
 				InfoToTxt(infoName);
 				ifstream readTxt(infoName);
@@ -333,8 +208,6 @@ int main()
 					readInfo.getline(moviename, 100);
 					writeResult2 << "(" << pDoc->docID << ","
 						<< moviename << ") ";
-					//cout << "(" << pDoc->docID << ","
-					//	<< moviename << ") ";
 					readInfo.close();
 					pDoc = pDoc->next;
 				}
@@ -351,6 +224,7 @@ int main()
 		if (!find)
 			writeResult2 << u8"该电影不在数据库中，无法推荐\n";
 	}
+	cout << "query 2 finished";
 
 	readQuery2.close();
 	writeResult2.close();
